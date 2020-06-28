f339669aaacad5c0fa4d17fa991593f982ff148f
tomcom147 <tomo1407@gmail.com>
2020-06-28 22:19:21 +0900

    init

---
+++ exec.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+
+docker exec -it python_dev /bin/bash
---
+++ gitinit.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+git config --global user.name 'tomcom147'
+git config --global user.email 'tomo1407@gmail.com'

d541488cd3fe250dca6a60bb9a184624c8aba11e
tomcom147 <tomo1407@gmail.com>
2020-06-29 00:40:49 +0900

    syuusei

--- docker_run.sh
+++ docker_run.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/bash
 
-cd docker
-docker build -t python_dev:1 .
-cd ..
+docker build -t python_dev:1 ./docker
 docker run --name python_dev -itd -v $(pwd)/src:/root/python/src python_dev:1 /bin/bash
 docker exec -it python_dev /bin/bash
